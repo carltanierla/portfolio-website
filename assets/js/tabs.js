8b582e0eadab79f8b00b60cbe558aa6c453dd0e7
Carl Joseph Tanierla <carltanierla12@gmail.com>
2024-10-22 22:50:37 +0800

    finished adding logos | added about me tabs

---
+++ assets/js/tabs.js
@@ -0,0 +1,20 @@
+const tabs = document.querySelectorAll('[data-target]'),
+tabContents = document.querySelectorAll('[data-content]');
+
+tabs.forEach((tab) => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector('#' + tab.dataset.target);
+        console.log(target);
+        tabContents.forEach((tabContent) => {
+            tabContent.classList.remove('tab__active');
+        });
+        
+        target.classList.add('tab__active');
+
+        tabs.forEach((tab) => {
+            tab.classList.remove('tab__active');
+        });
+
+        tab.classList.add('tab__active');
+    });
+})
